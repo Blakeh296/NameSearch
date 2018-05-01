8d675b5fb29188f9ca379ecfdd340bf6faa048a3
Blake Hoelle <bhoelle216@gmail.com>
2018-04-30 20:07:57 -0400

    Commit

--- NameSearch/NameSearch/Form1.Designer.cs
+++ NameSearch/NameSearch/Form1.Designer.cs
@@ -28,12 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.lbOutPut = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblNameSearch = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.Location = new System.Drawing.Point(176, 195);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(96, 23);
+            this.BtnSearch.TabIndex = 3;
+            this.BtnSearch.Text = "Search Name";
+            this.BtnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lbOutPut
+            // 
+            this.lbOutPut.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOutPut.FormattingEnabled = true;
+            this.lbOutPut.ItemHeight = 16;
+            this.lbOutPut.Location = new System.Drawing.Point(12, 90);
+            this.lbOutPut.Name = "lbOutPut";
+            this.lbOutPut.Size = new System.Drawing.Size(260, 100);
+            this.lbOutPut.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Info;
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadAllToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadAllToolStripMenuItem
+            // 
+            this.loadAllToolStripMenuItem.Name = "loadAllToolStripMenuItem";
+            this.loadAllToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadAllToolStripMenuItem.Text = "Load &All";
+            this.loadAllToolStripMenuItem.Click += new System.EventHandler(this.loadAllToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToolStripMenuItem.Text = "&Export";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(95, 195);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblNameSearch
+            // 
+            this.lblNameSearch.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.lblNameSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNameSearch.Location = new System.Drawing.Point(-1, 24);
+            this.lblNameSearch.Name = "lblNameSearch";
+            this.lblNameSearch.Size = new System.Drawing.Size(285, 31);
+            this.lblNameSearch.TabIndex = 0;
+            this.lblNameSearch.Text = "Name Search";
+            this.lblNameSearch.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbOutPut);
+            this.Controls.Add(this.BtnSearch);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblNameSearch);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.ListBox lbOutPut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblNameSearch;
     }
 }
 
--- NameSearch/NameSearch/Form1.cs
+++ NameSearch/NameSearch/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace NameSearch
 {
@@ -16,5 +17,221 @@ namespace NameSearch
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int counter = 0;
+
+            try
+            {
+                // Time variables 
+                DateTime start = DateTime.Now;
+                DateTime Finish;
+                TimeSpan Time;
+                // Array variables
+                const int SIZE = 5000;
+                string[] namesArray = new string[SIZE];
+                StreamReader inputFile = File.OpenText("Names.csv");
+                StreamWriter outputFile;
+                
+                // Input names into Names Array while counter is less than the number of items
+                while (counter < namesArray.Length && !inputFile.EndOfStream)
+                {
+                    namesArray[counter] = inputFile.ReadLine();
+                    counter++;
+                }
+
+                // Sort Array alphabetically 
+                Array.Sort(namesArray);
+                
+                // Close file connection
+                inputFile.Close();
+
+                // Output Full Array to the list box
+                foreach (string value in namesArray)
+                {
+                    lbOutPut.Items.Add(value);
+                }
+
+                // Save the time everything finished
+                Finish = DateTime.Now;
+                // Subtract the times to get the total time
+                Time = Finish - start;
+                // output time for the user
+                label1.Text = Time.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private int BinarySearch(string[] namesArray, int size, string value)
+        {
+            int first = 0;
+            int last = namesArray.Length - 1;
+            int middle;
+            int position = -1;
+            bool found = false;
+
+            while (!found && first <= last)
+            {
+                middle = (first + last) / 2;
+                if (namesArray[middle] == value)
+                {
+                    found = true;
+                    position = middle;
+                }
+                else if (string.Compare(namesArray[middle], value, false) > 0)
+                {
+                    last = middle - 1;
+                }
+                else
+                {
+                    first = middle + 1;
+                }
+
+            }
+
+            return position;
+
+        }
+        
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                string value = textBox1.Text;
+                int position;
+                string output;
+
+                const int SIZE = 5000;
+                string[] namesArray = new string[SIZE];
+                StreamReader inputFile = File.OpenText("Names.csv");
+
+                position = BinarySearch(namesArray, SIZE, value);
+
+                output = namesArray[position];
+
+                inputFile.Close();
+                Array.Sort(namesArray);
+                lbOutPut.Items.Add(output);
+                /*
+                foreach (string valuee in namesArray)
+                {
+                    lbOutPut.Items.Add(value);
+                }
+                */
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+
+            try
+            {
+                // Array variables
+                const int SIZE = 5000;
+                string[] namesArray = new string[SIZE];
+                StreamReader inputFile = File.OpenText("Names.csv");
+                StreamWriter outputFile;
+
+                // Input names into Names Array while counter is less than the number of items
+                while (counter < namesArray.Length && !inputFile.EndOfStream)
+                {
+                    namesArray[counter] = inputFile.ReadLine();
+                    counter++;
+                }
+
+                // Sort Array alphabetically 
+                Array.Sort(namesArray);
+
+                // Assign the file name to a variable
+                outputFile = File.CreateText("OutputNames.csv");
+
+                // While index < Array length, output array elements to the file
+                for (int index = 0; index < namesArray.Length; index++)
+                {
+                    outputFile.WriteLine(namesArray[index]);
+                }
+
+                // Close the file
+                inputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
+        }
+
+        private void loadAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+
+            try
+            {
+                // Time variables 
+                DateTime start = DateTime.Now;
+                DateTime Finish;
+                TimeSpan Time;
+                // Array variables
+                const int SIZE = 5000;
+                string[] namesArray = new string[SIZE];
+                StreamReader inputFile = File.OpenText("Names.csv");
+
+                // Input names into Names Array while counter is less than the number of items
+                while (counter < namesArray.Length && !inputFile.EndOfStream)
+                {
+                    namesArray[counter] = inputFile.ReadLine();
+                    counter++;
+                }
+
+                // Sort Array alphabetically 
+                Array.Sort(namesArray);
+
+                // Close file connection
+                inputFile.Close();
+
+                // Output Full Array to the list box
+                foreach (string value in namesArray)
+                {
+                    lbOutPut.Items.Add(value);
+                }
+
+                // Save the time everything finished
+                Finish = DateTime.Now;
+                // Subtract the times to get the total time
+                Time = Finish - start;
+                // output time for the user
+                label1.Text = Time.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbOutPut.Items.Clear();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
-}
+        
+    }
+
